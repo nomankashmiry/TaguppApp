ae90629fcd2b8af4f1a4dfade8158222db03a7db
noman arif <nomankashmiry@hotmail.com>
2020-06-07 23:27:46 +0500

    initial commit

---
+++ Tagupp.api/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Tagupp.api.Models;
+
+namespace Tagupp.api.Data
+{
+    public class DataContext : DbContext
+    {
+        
+    public DataContext(DbContextOptions<DataContext> options):base(options){}
+
+    public DbSet<Value> values { get; set; }
+    }
+}
---
+++ TaguppApp-spa/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { DummyComponent } from './dummy/dummy.component';
+
+
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      DummyComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
